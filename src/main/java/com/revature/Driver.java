db86cee60914d9f140967ed6ea025e3960ba4d40
imkewlhuh <rafael.lopez0827@gmail.com>
2023-06-09 00:04:05 -0400

    added main method and env variables

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,9 +1,12 @@
 package com.revature;
 
+import com.revature.utils.JavalinAppConfig;
+
 public class Driver {
 
     public static void main(String[] args) {
-
-
+        JavalinAppConfig app = new JavalinAppConfig();
+        app.start(8080);
     }
+
 }
